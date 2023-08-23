3ccd6df3e9f5f0674632443f82920bacd710ee9f
DhanpalNatraj <natrajdhanpal963@gmail.com>
2023-08-24 01:11:02 +0530

    new project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App (){
+  const [step, setStep] = useState(1);
+  const [open, setOpen] = useState(true)
+ 
+
+  function handlePrevious(){
+   if(step>1) setStep(step-1);
+  }
+  function handleNext(){
+    if(step<3) setStep(step+1);
+  }
+  return(
+  <div>
+    <button className="close" onClick={()=>setOpen(!open)}>&times;</button>
+    {open && ( <div className="steps">
+  <div className="numbers">
+    <div className={step >=1 ? 'active': ''}>1</div>
+    <div className={step >=2 ? 'active': ''}>2</div>
+    <div className={step >=3 ? 'active': ''}>3</div>
+  </div>
+  <p className="message">
+    Step {step}: {messages[step-1]}
+  </p>
+  <div className="buttons">
+  <button onClick={handlePrevious} style={{backgroundColor: '#7950f2', color:'#fff'}}>previous</button>
+  <button onClick={handleNext} style={{backgroundColor: '#7950f2', color:'#fff'}}>Next</button>
+  </div>
+  </div>)}</div>)
+}
